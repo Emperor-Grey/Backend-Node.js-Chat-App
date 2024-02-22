1e1ad0a95f7e5a615ff5398373327b05acfb42b0
King Grey <kinggrey9959@gmail.com>
2024-02-22 19:52:09 +0530

    Actually Connected to Socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socket = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+io.on("connection", () => {
+  console.log("New Websocket Connected");
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
