8224b18ff257e6006972d8ba74f1990bb4c25420
King Grey <kinggrey9959@gmail.com>
2024-02-23 20:00:24 +0530

    Added helpers functions(Partial) to better help

---
+++ src/utils/users.js
@@ -0,0 +1,47 @@
+const users = [];
+
+// Add users
+const addUser = ({ id, username, room }) => {
+  // Clear the Data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validating the Data
+  if (!username || !room) {
+    return {
+      error: "Username and room are Required",
+    };
+  }
+
+  // Name should be unique
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  //Validating username
+  if (existingUser) {
+    return {
+      error: "Username is in Use!!!",
+    };
+  }
+
+  // User is ready to be stored
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+// Remove users
+const removeUser = (id) => {
+  const index = users.findIndex((user) => {
+    return user.id === id;
+  });
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+// getUser
+
+// getUsersInRoom
